fd437f05b01ed3fc40f3578500fc1c0250567754
Salil Thakur <156768025+salilthakur9@users.noreply.github.com>
2025-03-25 15:26:42 +0530

    Add files via upload

---
+++ conditional_statement.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a=10,b=20;
+    if(a>b){
+        printf("a is greater than b");
+    }else{
+        printf("b is greater than a");
+    }
+    return 0;
+}
